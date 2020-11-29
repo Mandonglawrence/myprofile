c8d3d516ed10d74951a3acf8f468b6dd8436cc67
Lawrence Mandong <mandonglawrence@gmail.com>
2020-11-29 16:56:33 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+let counter = 0;
+let res = 1;
+// for (; counter < 20; counter++){
+//     res += 4;
+// }
+
+// console.log(res);
+
+
+while (counter < 20) {
+    
+    res+=4
+    counter++;
+}
+
+console.log(res);
---
+++ scripts/script.js
@@ -0,0 +1,33 @@
+const sect = document.querySelectorAll(".section1");
+const options = {
+  root: null,
+  threshold: 0.05,
+  rootMargin: "150px",
+};
+const observer = new IntersectionObserver(function (entries, observer) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      // document.querySelector('header').classList.remove('header');
+      return;
+    }
+    console.log(entry.target);
+    entry.target.classList.toggle("new");
+    observer.unobserve(entry.target);
+    // document.querySelector('header').classList.add('header');
+    // if(entry.isIntersecting === true){
+    //     console.log(entry.target);
+
+    //     entry.target.classList.toggle('new');
+    // }
+
+    // entry.target.classList.toggle('new');
+  });
+}, options);
+sect.forEach((aSection) => {
+  observer.observe(aSection);
+});
+let modeBtn = document.querySelector(".mode");
+modeBtn.addEventListener("click", (e) => {
+  document.querySelector(".screen").classList.toggle("dark");
+  // document.querySelector('.screen').classList.toggle('dark');
+});
